fac6219e76940e2d5587c5d359f92b1b04c60cad
nenadnesta <nenadnesta@gmail.com>
2020-01-10 20:16:07 +0100

    ASP .NET Core InProcess Hosting.

--- TheManager/Startup.cs
+++ TheManager/Startup.cs
@@ -27,7 +27,9 @@ namespace TheManager
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                //await context.Response.WriteAsync("Hello World!");
+                await context.Response
+                    .WriteAsync(System.Diagnostics.Process.GetCurrentProcess().ProcessName);
             });
         }
     }
