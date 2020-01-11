825379ac9ef14cdd299cd3b9e40eb906da7cb252
nenadnesta <nenadnesta@gmail.com>
2020-01-11 17:35:04 +0100

    ASP NET Core appsettings.json.

--- TheManager/Startup.cs
+++ TheManager/Startup.cs
@@ -5,12 +5,20 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace TheManager
 {
     public class Startup
     {
+        private IConfiguration _config;
+
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -28,8 +36,7 @@ namespace TheManager
             app.Run(async (context) =>
             {
                 //await context.Response.WriteAsync("Hello World!");
-                await context.Response
-                    .WriteAsync(System.Diagnostics.Process.GetCurrentProcess().ProcessName);
+                await context.Response.WriteAsync(_config["MyKey"]);
             });
         }
     }
