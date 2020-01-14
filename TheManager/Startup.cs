2fc6e62096710e0bf3aacb338692d13b99352b46
nenadnesta <nenadnesta@gmail.com>
2020-01-14 11:57:28 +0100

    ASP.NET Core Dependency Injection.

--- TheManager/Startup.cs
+++ TheManager/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using TheManager.Models;
 
 namespace TheManager
 {
@@ -25,6 +26,8 @@ namespace TheManager
         {
             services.AddMvc();
             //services.AddMvcCore();
+
+            services.AddSingleton<IEmployeeRepository, MockEmployeeRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
