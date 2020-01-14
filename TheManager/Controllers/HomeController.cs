1f2f9a78002b57e0eee8d56fc47df3a3a98a9c50
nenadnesta <nenadnesta@gmail.com>
2020-01-14 10:26:29 +0100

    Model in ASP.NET Core Mvc.

--- TheManager/Controllers/HomeController.cs
+++ TheManager/Controllers/HomeController.cs
@@ -3,13 +3,22 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TheManager.Models;
 
 namespace TheManager.Controllers
 {
     public class HomeController : Controller
     {
-        public JsonResult Index() {
-            return Json(new { id = 1, name = "Nenad" }); 
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public HomeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        public string Index()
+        {
+            return _employeeRepository.GetEmployee(1).Name;
         }
     }
 }
