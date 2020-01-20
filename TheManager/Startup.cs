ebc441adf8f40215d351424f1308ebdf87dcfa87
nenadnesta <nenadnesta@gmail.com>
2020-01-20 20:02:30 +0100

    Entity Framework Core - Using Sql Server.

--- TheManager/Startup.cs
+++ TheManager/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using TheManager.Models;
@@ -24,6 +25,9 @@ namespace TheManager
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContextPool<AppDbContext>(
+                options => options.UseSqlServer(_config.GetConnectionString("EmployeeDbConnection")));
+
             services.AddMvc().AddXmlSerializerFormatters();
             //services.AddMvcCore();
 
