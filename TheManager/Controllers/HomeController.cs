daf0dd493a5af89132c51eeab88129c7b2061983
nenadnesta <nenadnesta@gmail.com>
2020-01-20 12:20:02 +0100

    Model Validation in ASP.NET Core.

--- TheManager/Controllers/HomeController.cs
+++ TheManager/Controllers/HomeController.cs
@@ -50,10 +50,14 @@ namespace TheManager.Controllers
         }
 
         [HttpPost]
-        public RedirectToActionResult Create(Employee employee)
+        public IActionResult Create(Employee employee)
         {
-            Employee newEmployee = _employeeRepository.Add(employee);
-            return RedirectToAction("details", new { id = newEmployee.Id });
+            if (ModelState.IsValid) {
+                Employee newEmployee = _employeeRepository.Add(employee);
+                return RedirectToAction("details", new { id = newEmployee.Id });
+            }
+
+            return View();
         }
     }
 }
--- TheManager/Models/Employee.cs
+++ TheManager/Models/Employee.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +9,16 @@ namespace TheManager.Models
     public class Employee
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public Dept Department { get; set; }
 
+        [Required]
+        [MaxLength(50, ErrorMessage = "Name cannot exceed 50 characters")]
+        public string Name { get; set; }
+
+        [Display(Name = "Office Email")]
+        [Required]
+        [RegularExpression(@"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$", ErrorMessage = "Invalid email format")]
+        public string Email { get; set; }
+
+        public Dept Department { get; set; }
     }
 }
