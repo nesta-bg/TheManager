bc3616641c646216c24139346d9a76cfbce2cbd5
nenadnesta <nenadnesta@gmail.com>
2020-01-28 14:25:54 +0100

    Nlog Logging to File in ASP.NET Core.

--- TheManager/Program.cs
+++ TheManager/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
 
 namespace TheManager
 {
@@ -19,6 +20,14 @@ namespace TheManager
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .ConfigureLogging((hostingContext, logging) =>
+                {
+                    logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                    logging.AddConsole();
+                    logging.AddDebug();
+                    logging.AddEventSourceLogger();
+                    logging.AddNLog();
+                })
                 .UseStartup<Startup>();
     }
 }
