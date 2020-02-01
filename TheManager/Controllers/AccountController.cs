f6c379cb205b665194d5de2e4dd67da51220c267
nenadnesta <nenadnesta@gmail.com>
2020-02-01 18:47:55 +0100

    ASP.NET Core Login Functionality.

--- TheManager/Controllers/AccountController.cs
+++ TheManager/Controllers/AccountController.cs
@@ -66,5 +66,32 @@ namespace TheManager.Controllers
             await signInManager.SignOutAsync();
             return RedirectToAction("index", "home");
         }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                //PasswordSignInAsync - Go To Definition
+                //session cookie (is immediately lose after the browser window is closed) or persistent cookie (stay on the client machine after the browser window is closed)
+                var result = await signInManager.PasswordSignInAsync(
+                    model.Email, model.Password, model.RememberMe, false);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("index", "home");
+                }
+
+                ModelState.AddModelError(string.Empty, "Invalid Login Attempt");
+            }
+
+            return View(model);
+        }
     }
 }
