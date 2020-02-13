7a959bb5622e24d0393c50b27fb05db9c797b5a4
nenadnesta <nenadnesta@gmail.com>
2020-02-13 20:01:23 +0100

    EF CORE Enforce Foreign Keys ON DELETE NO ACTION.

--- TheManager/Models/AppDbContext.cs
+++ TheManager/Models/AppDbContext.cs
@@ -22,6 +22,11 @@ namespace TheManager.Models
         {
             base.OnModelCreating(modelBuilder);
             modelBuilder.Seed();
+
+            foreach (var foreignKey in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+            {
+                foreignKey.DeleteBehavior = DeleteBehavior.Restrict;
+            }
         }
     }
 }
