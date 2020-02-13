53c4e200dafa3681e2f29f22006e158eb2fda626
nenadnesta <nenadnesta@gmail.com>
2020-02-13 16:53:10 +0100

    ASP.NET Core Delete Identity Role.

--- TheManager/Controllers/AdministrationController.cs
+++ TheManager/Controllers/AdministrationController.cs
@@ -297,5 +297,33 @@ namespace TheManager.Controllers
 
             return RedirectToAction("EditRole", new { Id = roleId });
         }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteRole(string id)
+        {
+            var role = await roleManager.FindByIdAsync(id);
+
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"Role with Id = {id} cannot be found";
+                return View("NotFound");
+            }
+            else
+            {
+                var result = await roleManager.DeleteAsync(role);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("ListRoles");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+
+                return View("ListRoles");
+            }
+        }
     }
 }
